5af7b24cb6f2868a0819a70d812f2fd398f5aaa6
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-11-10 13:11:27 +0100

    added test to convert html from singlenoteview

---
+++ spec/single-note-view-spec.js
@@ -0,0 +1,7 @@
+describe('SingleNoteView', function() {
+    var note = new Note("I can show you the World")
+    var singlenoteview = new SingleNoteView(note);
+    var html = singlenoteview.convertToHtml(note);
+    expect(html).toEqual("<div> I can show you the World </div>")
+
+})
